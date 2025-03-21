f8a18db42f0349c9c49dcf03df06cb5986c11633
matkv <mail@matkv.dev>
2025-03-21 13:19:07 +0100

    small styling changes

--- src/routes/addBook/+page.svelte
+++ src/routes/addBook/+page.svelte
@@ -67,16 +67,24 @@
         <input type="text" bind:value={title} placeholder="Title" required />
         <input type="text" bind:value={author} placeholder="Author" required />
         <input type="number" bind:value={pages} placeholder="Pages" required />
-        <button type="submit">Add Book</button>
+        <button
+            style="background-color: lightgrey; color: white; border: none; padding: 0.5rem; border-radius: 4px; cursor: pointer;"
+            type="submit">Add Book</button
+        >
     </form>
 
     <!-- Display the list of books -->
     <h2 style="margin-bottom: 1rem;margin-top: 1rem;">Books List</h2>
     <ul>
         {#each books as book}
-            <li>
+            <li style="margin-bottom: 0.5rem;">
                 <b>{book.id} {book.title}</b> by {book.author} ({book.pages} pages)
-                <button on:click={() => deleteBook(book.id)}>Delete</button>
+                <button
+                    on:click={() => deleteBook(book.id)}
+                    style="background-color: lightgrey; color: white; border: none; padding: 0.5rem 1rem; border-radius: 4px; cursor: pointer;"
+                >
+                    Delete
+                </button>
             </li>
         {/each}
     </ul>
